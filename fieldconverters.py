0bcc563496775bce7db984bf5ed611dd579f39f0
Jeremy Hylton <jeremy@svn.zope.org>
2003-04-25 19:28:45 +0000

    Add missing copyright notices.

--- fieldconverters.py
+++ fieldconverters.py
@@ -1,6 +1,19 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 """Zope-specific request field converters.
 
-$Id: fieldconverters.py,v 1.1 2003/02/13 17:46:19 tseaver Exp $
+$Id: fieldconverters.py,v 1.2 2003/04/25 19:28:44 jeremy Exp $
 """
 from datetime import datetime
 
