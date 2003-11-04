afaa0fa31f432eed4d492a70665a79a4b381b727
Jeremy Hylton <jeremy@svn.zope.org>
2003-11-04 04:04:28 +0000

    Fix various import issues.
    Some names where from imported twice.
    Other imports were unused.
    Often imports were in partly random order, making them harder to read.

--- fileresource.py
+++ fileresource.py
@@ -13,37 +13,32 @@
 ##############################################################################
 """
 
-$Id: fileresource.py,v 1.3 2003/05/01 19:35:27 faassen Exp $
+$Id: fileresource.py,v 1.4 2003/11/04 04:04:26 jeremy Exp $
 """
-__metaclass__ = type # All classes are new style when run with Python 2.2+
-
-
 from zope.app.content_types import guess_content_type
 from zope.app.datetimeutils import rfc1123_date
 from time import time
-from os import stat
 import os
 
-class File:
+class File(object):
+    
+    def __init__(self, path):
+        self.path = path
+
+        f = open(path, 'rb')
+        data = f.read()
+        f.close()
+        self.content_type, enc = guess_content_type(path, data)
+        self.__name__ = path[path.rfind('/') + 1:]
+        self.lmt = float(os.stat(path)[8]) or time()
+        self.lmh = rfc1123_date(self.lmt)
+
+class Image(File):
     """Image objects stored in external files."""
 
     def __init__(self, path):
-
-        self.path=path
-
-        file=open(path, 'rb')
-        data=file.read()
-        file.close()
-        self.content_type, enc = guess_content_type(path, data)
-        self.__name__=path[path.rfind('/')+1:]
-        self.lmt=float(stat(path)[8]) or time()
-        self.lmh=rfc1123_date(self.lmt)
-
-class Image(File):
-
-    def __init__(self, path):
         super(Image, self).__init__(path)
-        if self.content_type in (None,  'application/octet-stream'):
+        if self.content_type in (None, 'application/octet-stream'):
             ext = os.path.splitext(self.path)[1]
             if ext:
-                self.content_type='image/%s' % ext[1:]
+                self.content_type = 'image/%s' % ext[1:]
