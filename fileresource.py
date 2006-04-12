e14f8d9c50b4b028919006b64b7393cc9a0c04eb
Philipp von Weitershausen <philipp@weitershausen.de>
2006-04-12 19:19:25 +0000

    Move zope.app.content_types not to zope.app.contenttypes but to zope.contenttype.

--- fileresource.py
+++ fileresource.py
@@ -17,10 +17,9 @@ $Id$
 """
 import os
 import posixpath
-
 from time import time
 
-from zope.app.contenttypes import guess_content_type
+from zope.contenttype import guess_content_type
 from zope.datetime import rfc1123_date
 
 
