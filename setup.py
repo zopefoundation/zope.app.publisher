563fe411ab839a837e5527ba9e5f5b40cde1c5a4
Fred Drake <fdrake@acm.org>
2007-03-25 09:43:40 +0000

    - buildoutify - fix up dependencies

--- setup.py
+++ setup.py
@@ -3,16 +3,25 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='zope.app.publisher',
-      version='0.1dev',
+      version='0.1',
       url='http://svn.zope.org/zope.app.publisher',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
 
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
 
       namespace_packages=['zope', 'zope.app'],
-      include_package_data = True,
+      include_package_data=True,
+      install_requires=[
+          'setuptools',
+          'zope.component',
+          'zope.interface',
+          'zope.publisher',
+          ],
+      extras_require={
+          'test': ['zope.testing', 'zope.app.testing'],
+          },
 
       zip_safe = False,
       )
