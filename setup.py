6fea831028a74258355359fd0bf732d7e85ec2b5
Philipp von Weitershausen <philipp@weitershausen.de>
2007-08-23 21:29:42 +0000

    Merge from 3.4 branch:
    ------------------------------------------------------------------------
    r79177 | philikon | 2007-08-23 23:07:09 +0200 (Thu, 23 Aug 2007) | 2 lines
    
    Remove unused import and add license header
    
    ------------------------------------------------------------------------

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
-import os
-
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""zope.app.publisher setup
+"""
 from setuptools import setup, find_packages, Extension
 
 setup(name='zope.app.publisher',
