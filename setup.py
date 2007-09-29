6b3ad949214ef545ebb0189ab92be37d2a2b1947
Philipp von Weitershausen <philipp@weitershausen.de>
2007-09-29 10:46:49 +0000

    Get ready for release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (open('README.txt').read() + '\n\n' +
                     open('CHANGES.txt').read())
 
 setup(name='zope.app.publisher',
-      version = '3.4.0b2',
+      version = '3.4.0',
       url='http://pypi.python.org/pypi/zope.app.publisher',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
