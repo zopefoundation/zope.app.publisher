f4b3709220ebe24ef19f40c8490b8710e2eedad1
Stephan Richter <stephan.richter@gmail.com>
2007-10-31 19:20:54 +0000

    Fix deprecation warning.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (open('README.txt').read() + '\n\n' +
                     open('CHANGES.txt').read())
 
 setup(name='zope.app.publisher',
-      version = '3.4.1dev',
+      version = '3.4.1',
       url='http://pypi.python.org/pypi/zope.app.publisher',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
